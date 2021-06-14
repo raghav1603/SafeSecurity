890c911a081205b68bb16e82d83a65ff90582f25
raghav1603 <raghavmishra.1603@gmail.com>
2021-06-14 19:34:04 +0530

    Readme edit

--- rq8kr/src/components/PostData.js
+++ rq8kr/src/components/PostData.js
@@ -4,11 +4,13 @@ import CsvDownloader from "react-csv-downloader";
 import FormData from "./Form";
 
 function PostData() {
+  // initialise store with a default value to check if there is successful ppost happening on the given port
   const [formData, setFormData] = useState({
     name:"test",
     age:'23',
     dob:"2021-06-03"
   });
+  // effect will evoke everytime there is change in formdata
   useEffect(() => {
     console.log(formData)
     axios.post('http://localhost:4000/EmpData',formData)
@@ -18,7 +20,7 @@ function PostData() {
     .catch((e)=>{
       console.error("encountered error",e.message)
     })
-  }, []);
+  }, [formData]);
   return (
     <div>
       <FormData setFormData={setFormData} />
